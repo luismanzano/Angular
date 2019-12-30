bd4a969191bbd1027131d2e5807a700f9f074907
Luis Fernando <luis.manzano@correo.unimet.edu.ve>
2019-12-30 19:22:28 -0400

    Aprendi Data binding, Prooperty Binding y Two Way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platziSquare';
+  a = 'Esto es una prueba de String Interpolation';
+  b = 5;
+  c = 3;
+  listo = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.listo = true;
+      }, 3000)
+  }
+
+  hacerAlgo(){
+    alert("Haciendo Algo");
+  }
 }
